263963f29d424319be1d8a5a371163c2226dd172
Roberto Serrano <r.semarcos@gmail.com>
2022-04-10 04:11:45 +0200

    Setup minimal webhook with http4s

--- src/main/scala/com/lambdarat/teleborm/Main.scala
+++ src/main/scala/com/lambdarat/teleborm/Main.scala
@@ -1,10 +1,19 @@
 package com.lambdarat.teleborm
 
+import cats.effect.ExitCode
+import cats.effect.IO
 import cats.effect.IOApp
-import cats.effect.{ExitCode, IO}
-import cats.syntax.all._
+import com.lambdarat.teleborm.bot.TelebormBot
+import sttp.client3.asynchttpclient.fs2.AsyncHttpClientFs2Backend
 
 object Main extends IOApp {
-  def run(args: List[String]): IO[ExitCode] =
-    IO.println("Hello world!").as(ExitCode.Success)
+  def run(args: List[String]): IO[ExitCode] = {
+    AsyncHttpClientFs2Backend.resource[IO]().use { client =>
+      val token      = "<add_your_token>"
+      val webhookUrl = "<add_your_url>"
+      val bot        = new TelebormBot[IO](client, token, webhookUrl)
+
+      bot.run().as(ExitCode.Success)
+    }
+  }
 }
---
+++ src/main/scala/com/lambdarat/teleborm/bot/TelebormBot.scala
@@ -0,0 +1,52 @@
+package com.lambdarat.teleborm.bot
+
+import cats.effect.kernel.Async
+import cats.syntax.all._
+import com.bot4s.telegram.api.declarative.Commands
+import com.bot4s.telegram.cats.TelegramBot
+import com.bot4s.telegram.marshalling._
+import com.bot4s.telegram.methods.SetWebhook
+import com.bot4s.telegram.models.Update
+import com.bot4s.telegram.models.User
+import com.comcast.ip4s._
+import org.http4s._
+import org.http4s.dsl.Http4sDsl
+import org.http4s.ember.server._
+import org.http4s.implicits._
+import sttp.client3.SttpBackend
+
+class TelebormBot[F[_]: Async](backend: SttpBackend[F, _], token: String, webhookUrl: String)
+    extends TelegramBot[F](token, backend)
+    with Commands[F] {
+
+  private val dsl = new Http4sDsl[F] {}
+  import dsl._
+
+  val webhookHandler = HttpRoutes
+    .of[F] { case req @ POST -> Root =>
+      for {
+        body     <- req.bodyText.compile.string
+        update   <- Async[F].delay(fromJson[Update](body))
+        _        <- receiveUpdate(update, none[User])
+        response <- Ok()
+      } yield response
+    }
+    .orNotFound
+
+  val webhookServer: F[Unit] = EmberServerBuilder
+    .default[F]
+    .withHost(ipv4"0.0.0.0")
+    .withPort(port"9000")
+    .withHttpApp(webhookHandler)
+    .build
+    .use(_ => Async[F].never)
+
+  override def run(): F[Unit] = {
+    val setWebhookUrlRequest = request(SetWebhook(url = webhookUrl))
+
+    Async[F].ifM(setWebhookUrlRequest)(
+      webhookServer,
+      Async[F].raiseError(new Exception(s"Could not set webhook URL: ${webhookUrl}"))
+    )
+  }
+}
