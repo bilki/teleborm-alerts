18bc3a651af9fb70dcb60a17c3339a2264f7538c
Roberto Serrano <r.semarcos@gmail.com>
2022-04-15 20:58:46 +0200

    Add support to extract search command from callback

--- src/main/scala/com/lambdarat/teleborm/bot/BormCommand.scala
+++ src/main/scala/com/lambdarat/teleborm/bot/BormCommand.scala
@@ -2,11 +2,33 @@ package com.lambdarat.teleborm.bot
 
 import java.time.LocalDate
 
+import cats.syntax.all._
+
 sealed abstract class BormCommand
 
 object BormCommand {
-  case class SaveAlert(userId: Long, words: List[String])         extends BormCommand
-  case class DeleteAlert(userId: Long, alertId: Long)             extends BormCommand
-  case class MyAlerts(userId: Long)                               extends BormCommand
-  case class Search(words: List[String], from: Option[LocalDate]) extends BormCommand
+  case class SaveAlert(userId: Long, words: List[String]) extends BormCommand
+  case class DeleteAlert(userId: Long, alertId: Long)     extends BormCommand
+  case class MyAlerts(userId: Long)                       extends BormCommand
+  case class Search(words: List[String], page: Option[Int], from: Option[LocalDate])
+      extends BormCommand
+
+  def extractFrom(cbData: Option[String]): Option[BormCommand] = {
+    val maybeBormCommand = for {
+      rawData <- cbData
+      rawParts = rawData.split(":")
+      rawCommandType <- rawParts.headOption
+      commandType    <- BormCommandType.withNameOption(rawCommandType)
+    } yield commandType match {
+      case BormCommandType.Search =>
+        rawParts.toList.tail match {
+          case page :: rawWords :: Nil =>
+            Search(rawWords.split(",").toList, page.toIntOption, none[LocalDate]).some
+          case _ => none[BormCommand]
+        }
+      case _ => none[BormCommand]
+    }
+
+    maybeBormCommand.flatten
+  }
 }
